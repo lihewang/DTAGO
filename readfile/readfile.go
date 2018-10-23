15034874c1e8c8e7b41ace342b8df99148d43151
lihewang <lihe_wang@hotmail.com>
2018-10-23 15:11:35 -0400

    Add files via upload

--- readfile/readfile.go
+++ readfile/readfile.go
@@ -3,6 +3,7 @@ package readfile
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"strconv"
@@ -12,7 +13,7 @@ import (
 
 //ReadPar Read Parameters
 func ReadPar(par *tp.Par) {
-	CtrlFile := "C:/Users/lihe.wang/go/input/eltod.ctl"
+	CtrlFile := "eltod.ctl"
 	file, err := os.Open(CtrlFile)
 	if err != nil {
 		log.Fatal(err)
@@ -23,23 +24,50 @@ func ReadPar(par *tp.Par) {
 		key := strings.TrimSpace(scanner.Text()[:40])
 		value := strings.TrimSpace(scanner.Text()[40:])
 		parTemp[key] = value
-		//fmt.Println(key + ".." + par[key])
+		//fmt.Println(key + ".." + parTemp[key])
 	}
 	par.Link = parTemp["LINK_FILE"]
 	par.Node = parTemp["NODE_FILE"]
 	par.TripTable = parTemp["TRIP_FILE"]
-	par.NodeTypeZone = ParsetoArray(parTemp["ZONE_NODE_TYPE"])
-	par.NodeTypeEntry = ParsetoArray(parTemp["EXPRESS_ENTRY_TYPES"])
-	par.NodeTypeExit = ParsetoArray(parTemp["EXPRESS_EXIT_TYPES"])
+	par.TollConstFile = parTemp["TOLL_CONSTANT_FILE"]
+	par.ChoiceLogFile = parTemp["NEW_MODEL_DATA_FILE"]
+	par.NodeTypeZone = ParsetoInt(parTemp["ZONE_NODE_TYPE"])
+	par.NodeTypeEntry = ParsetoArrayInt(parTemp["EXPRESS_ENTRY_TYPES"])
+	par.NodeTypeExit = ParsetoArrayInt(parTemp["EXPRESS_EXIT_TYPES"])
+	par.LinkTypeEL = ParsetoArrayInt(parTemp["EXPRESS_FACILITY_TYPES"])
+	par.ELEntry = ParsetoInt(parTemp["EXPRESS_LINK_ENTRY_TYPE"])
+	par.ELExit = ParsetoInt(parTemp["EXPRESS_LINK_EXIT_TYPE"])
 	par.NumThreads = ParsetoInt(parTemp["NUMBER_OF_THREADS"])
-	par.VOT = ParseRecord(parTemp["COST_VALUE"])
+	par.MaxIter = ParsetoInt(parTemp["MAXIMUM_ITERATIONS"])
+	par.VOT = ParsetoArrayFloat(parTemp["COST_VALUE"])
+	par.MinSplit = ParsetoFloat(parTemp["MINIMUM_TRIP_SPLIT"])
+	par.SmoothForwardPercent = ParsetoInt(parTemp["PERCENT_MOVED_FORWARD"])
+	par.SmoothBackworkPercent = ParsetoInt(parTemp["PERCENT_MOVED_BACKWARD"])
+	par.SmoothIter = ParsetoInt(parTemp["SMOOTHING_ITERATIONS"])
+
+	//choice model par
+	par.CMTime = ParsetoArrayFloat(parTemp["MODEL_TIME_FACTOR"])
+	par.CMToll = ParsetoArrayFloat(parTemp["MODEL_TOLL_FACTOR"])
+	par.CMReliaRatio = ParsetoFloat(parTemp["MODEL_RELIABILITY_RATIO"])
+	par.CMReliaTime = ParsetoFloat(parTemp["MODEL_RELIABILITY_TIME"])
+	par.CMReliaDist = ParsetoFloat(parTemp["MODEL_RELIABILITY_DISTANCE"])
+	par.CMPercTime = ParsetoFloat(parTemp["MODEL_PERCEIVED_TIME"])
+	par.CMPercMidVC = ParsetoFloat(parTemp["MODEL_PERCEIVED_MID_VC"])
+	par.CMPercMaxVC = ParsetoFloat(parTemp["MODEL_PERCEIVED_MAX_VC"])
+	par.CMELWeight = ParsetoFloat(parTemp["MODEL_EXPRESS_WEIGHT"])
+	par.CMScaleLen = ParsetoFloat(parTemp["MODEL_SCALE_LENGTH"])
+	par.CMMaxCir = ParsetoFloat(parTemp["MODEL_MAX_CIRCUITY"])
+	par.CLogTS = ParsetoArrayInt(parTemp["SELECT_MODEL_PERIODS"])
+	par.CLogIter = ParsetoArrayInt(parTemp["SELECT_MODEL_ITERATIONS"])
+	par.CLogNode = ParsetoArrayInt(parTemp["SELECT_MODEL_NODES"])
+	par.CLogVCLS = ParseFields(parTemp["SELECT_MODEL_MODES"])
 
 	//parse toll policy
-	tpCodes := ParsetoArray(parTemp["TOLL_POLICY_CODES"])
-	tpMin := ParseRecord(parTemp["MINIMUM_TOLL"])
-	tpMax := ParseRecord(parTemp["MAXIMUM_TOLL"])
-	tpVCOffset := ParseRecord(parTemp["VC_RATIO_OFFSET"])
-	tpExp := ParseRecord(parTemp["TOLL_EXPONENT"])
+	tpCodes := ParsetoArrayInt(parTemp["TOLL_POLICY_CODES"])
+	tpMin := ParsetoArrayFloat(parTemp["MINIMUM_TOLL"])
+	tpMax := ParsetoArrayFloat(parTemp["MAXIMUM_TOLL"])
+	tpVCOffset := ParsetoArrayFloat(parTemp["VC_RATIO_OFFSET"])
+	tpExp := ParsetoArrayFloat(parTemp["TOLL_EXPONENT"])
 	par.TollPolicy = make(map[int]map[string]float64)
 	for i := range tpCodes {
 		plc := make(map[string]float64)
@@ -51,7 +79,36 @@ func ReadPar(par *tp.Par) {
 	}
 
 	file.Close()
-	fmt.Println("Control file: " + CtrlFile)
+
+	//read toll constant
+	file, err = os.Open(par.TollConstFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner = bufio.NewScanner(file)
+	i := 0
+	var NodeID float64
+	var cst []float64
+	cstpar := make(map[int][]float64)
+	NodeID = -1
+	for scanner.Scan() { // internally, it advances token based on sperator
+		if i > 0 {
+			txt := strings.Split(scanner.Text(), ",")
+			n, _ := strconv.ParseFloat(strings.TrimSpace(txt[0]), 64)
+			value, _ := strconv.ParseFloat(strings.TrimSpace(txt[2]), 64)
+			if NodeID != n {
+				if i > 1 {
+					cstpar[int(NodeID)] = cst
+					cst = nil
+				}
+				NodeID = n
+			}
+			cst = append(cst, value)
+		}
+		i++
+	}
+	cstpar[int(NodeID)] = cst
+	par.TollConst = cstpar
 }
 
 //ParsetoInt parse parameters
@@ -60,8 +117,14 @@ func ParsetoInt(s string) (par int) {
 	return int(p)
 }
 
-//ParsetoArray parse parameters
-func ParsetoArray(s string) (par []int) {
+//ParsetoFloat parse parameters
+func ParsetoFloat(s string) (par float64) {
+	par, _ = strconv.ParseFloat(s, 64)
+	return
+}
+
+//ParsetoArrayInt parse parameters
+func ParsetoArrayInt(s string) (par []int) {
 	for _, element := range strings.Split(s, ",") {
 		p, _ := strconv.ParseInt(element, 10, 0)
 		par = append(par, int(p))
@@ -70,10 +133,10 @@ func ParsetoArray(s string) (par []int) {
 }
 
 //ReadNode read node file
-func ReadNode(par *tp.Par, nodes map[int]*tp.Node) {
+func ReadNode(par *tp.Par, nodes map[int]*tp.Node, wr io.Writer) {
 	file, err := os.Open(par.Node)
 	//get zone type
-	ztp := par.NodeTypeZone[0]
+	ztp := par.NodeTypeZone
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -86,16 +149,35 @@ func ReadNode(par *tp.Par, nodes map[int]*tp.Node) {
 		if i > 0 {
 			txt := strings.Split(scanner.Text(), ",")
 			n, err := strconv.ParseInt(strings.TrimSpace(txt[0]), 10, 32)
+			x := ParsetoFloat(strings.TrimSpace(txt[1]))
+			y := ParsetoFloat(strings.TrimSpace(txt[2]))
 			ndtype, err := strconv.ParseInt(strings.TrimSpace(txt[3]), 10, 8)
 			dngrp, err := strconv.ParseInt(strings.TrimSpace(txt[4]), 10, 8)
 			if err != nil {
 				log.Fatal(err)
 			}
-
+			elentry := false
+			for _, v := range par.NodeTypeEntry {
+				if int(ndtype) == v {
+					elentry = true
+					break
+				}
+			}
+			elexit := false
+			for _, v := range par.NodeTypeExit {
+				if int(ndtype) == v {
+					elexit = true
+					break
+				}
+			}
 			node := tp.Node{
-				N:     int(n),
-				TYPE:  int(ndtype),
-				DNGRP: int(dngrp),
+				N:         int(n),
+				X:         x,
+				Y:         y,
+				TYPE:      int(ndtype),
+				DNGRP:     int(dngrp),
+				IsELEntry: elentry,
+				IsELExit:  elexit,
 			}
 			if node.TYPE == ztp {
 				if numzones < node.N {
@@ -104,15 +186,15 @@ func ReadNode(par *tp.Par, nodes map[int]*tp.Node) {
 			}
 			nodes[int(n)] = &node
 		}
-		i = i + 1
+		i++
 	}
 	par.NumZones = numzones //number of zones
 	file.Close()
-	fmt.Printf("Read node file. Total of %+v zones.\n", numzones)
+	fmt.Fprintf(wr, "Read node file. Total of %+v zones.\n", numzones)
 }
 
 //ReadLink read link file
-func ReadLink(par *tp.Par, links map[int]*tp.Link, nodes map[int]*tp.Node) {
+func ReadLink(par *tp.Par, links map[int]*tp.Link, nodes map[int]*tp.Node, wr io.Writer) {
 	file, err := os.Open(par.Link)
 	if err != nil {
 		log.Fatal(err)
@@ -146,11 +228,22 @@ func ReadLink(par *tp.Par, links map[int]*tp.Link, nodes map[int]*tp.Node) {
 				}
 			}
 			//CgTime
-			var cgtimes []float64
-			var tolls []float64
-			for j := 1; j <= 96; j++ {
-				cgtimes = append(cgtimes, dist/ffspeed*60)
-				tolls = append(tolls, toll)
+			var cgtimes [96]float64
+			var tolls [96]float64
+			var weight [96]float64
+			for j := 0; j <= 95; j++ {
+				cgtimes[j] = dist / ffspeed * 60
+				tolls[j] = toll
+				weight[j] = 1
+			}
+			//EL
+			elentry := false
+			if int(ftype) == par.ELEntry {
+				elentry = true
+			}
+			elexit := false
+			if int(ftype) == par.ELExit {
+				elexit = true
 			}
 			link := tp.Link{
 				ID:          i,
@@ -171,27 +264,67 @@ func ReadLink(par *tp.Par, links map[int]*tp.Link, nodes map[int]*tp.Node) {
 				NODEB:       nodes[int(b)],
 				CgTime:      cgtimes,
 				FFTime:      dist / ffspeed * 60,
-				TimeWeight:  1,
+				TimeWeight:  weight,
 				TollRate:    tolls,
+				IsELEntry:   elentry,
+				IsELExit:    elexit,
+				VolCls:      make(map[string]*[96]float64),
+				VolClsPre:   make(map[string]*[96]float64),
 			}
-
+			//init link vols
+			modes := []string{"AUTOVOT1", "AUTOVOT2", "AUTOVOT3", "AUTOVOT4", "AUTOVOT5", "TRK"}
+			for _, cls := range modes {
+				var vol [96]float64
+				var volpre [96]float64
+				link.VolCls[cls] = &vol
+				link.VolClsPre[cls] = &volpre
+			}
 			links[i] = &link
 			nodes[int(a)].DNLINKS = append(nodes[int(a)].DNLINKS, &link) //add down links to node A
 		}
 		i = i + 1
 	}
 	file.Close()
-	fmt.Printf("Read link file. Total of %+v links.\n", i-1)
+	fmt.Fprintf(wr, "Read link file. Total of %+v links.\n", i-1)
+}
+
+//CheckNetwork check network coding
+func CheckNetwork(par *tp.Par, links map[int]*tp.Link, nodes map[int]*tp.Node, wr io.Writer) {
+	//decision nodes need at least two downstream links
+	fatal := 0
+	numDNd := 0
+	for _, nd := range nodes {
+		if nd.TYPE == par.NodeTypeEntry[0] || nd.TYPE == par.NodeTypeExit[0] {
+			HasRamp := false
+			numDNd++
+			if len(nd.DNLINKS) < 2 {
+				fmt.Fprintf(wr, "!Fatal network coding error: decision node %v has no alternative path.\n", nd.N)
+				fatal++
+			}
+			for _, dlnk := range nd.DNLINKS {
+				if dlnk.IsELEntry || dlnk.IsELExit {
+					HasRamp = true
+				}
+			}
+			if !HasRamp {
+				fmt.Fprintf(wr, "!Fatal network coding error: decision node %v has no express path.\n", nd.N)
+				fatal++
+			}
+		}
+	}
+	fmt.Fprintf(wr, "Number of decision node %v.\n", numDNd)
+	if fatal > 0 {
+		os.Exit(1)
+	}
 }
 
 //ReadTT Read trip table
-func ReadTT(par *tp.Par, nodes map[int]*tp.Node, TaskQueue chan *tp.PathTask) {
+func ReadTT(par *tp.Par, nodes map[int]*tp.Node, TaskQueue chan *tp.PathTask, wr io.Writer) {
 	file, err := os.Open(par.TripTable)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
-
 	var i int
 	var ts, iNodeID float64 //time step
 	jNodes := []*tp.Node{}
@@ -199,36 +332,45 @@ func ReadTT(par *tp.Par, nodes map[int]*tp.Node, TaskQueue chan *tp.PathTask) {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		if i == 0 { //first record, field names
-			Record := ParseFields(scanner.Text())
-			for j, element := range Record[3:] {
+			line := ParseFields(scanner.Text())
+			//Put pars into maps
+			par.VOTFactor = make(map[string]float64)
+			par.CMTimeFactor = make(map[string]float64)
+			par.CMTollFactor = make(map[string]float64)
+			for j, element := range line[3:] {
 				par.VehClass = append(par.VehClass, element)
-				//VOT factor by veh class
-				par.VOTFactor = make(map[string]float64)
 				par.VOTFactor[element] = par.VOT[j]
+				par.CMTimeFactor[element] = par.CMTime[j]
+				par.CMTollFactor[element] = par.CMToll[j]
 			}
-			fmt.Printf("Read trip table file. Vehicle classes %+v\n", par.VehClass)
 		} else { //data
-			Record := ParseRecord(scanner.Text()) //parsed to float64
+			Record := ParsetoArrayFloat(scanner.Text()) //parsed to float64
 			tsCurr := Record[2]
 			iNodeIDCurr := Record[0]
 			if i > 1 && (ts != tsCurr || iNodeID != iNodeIDCurr) { //diffrent origin node or time step
-				//create task for each vehicle class
-				for col, element := range par.VehClass {
-					trips := make(map[*tp.Node]float64)
-					for j := 0; j < len(tripRecs); j++ {
-						if tripRecs[j][col] > 0 {
-							trips[jNodes[j]] = tripRecs[j][col]
+				if iNodeIDCurr != Record[1] { //exclude intra zonal trips
+					//create task for each vehicle class
+					for col, element := range par.VehClass {
+						trips := make(map[*tp.Node]float64)
+						for j := 0; j < len(tripRecs); j++ {
+							if tripRecs[j][col] > 0 {
+								trips[jNodes[j]] = tripRecs[j][col]
+							}
 						}
-					}
-					if len(trips) > 0 {
-						task := tp.PathTask{
-							TP:   int(ts),
-							I:    nodes[int(iNodeID)],
-							VCLS: element,
-							TRIP: trips,
+						if len(trips) > 0 {
+							task := tp.PathTask{
+								TP:     int(ts),
+								I:      nodes[int(iNodeID)],
+								O:      nodes[int(iNodeID)],
+								VCLS:   element,
+								TRIP:   trips,
+								Rcd:    i,
+								LShare: 0,
+								HShare: 1,
+							}
+							TaskQueue <- &task
+							//fmt.Printf("task queue length %+v\n", len(TaskQueue))
 						}
-						TaskQueue <- &task //send task to task queue
-						//fmt.Printf("task sent to queue %+v\n", task)
 					}
 				}
 				jNodes = nil
@@ -246,16 +388,16 @@ func ReadTT(par *tp.Par, nodes map[int]*tp.Node, TaskQueue chan *tp.PathTask) {
 			tripRecs = append(tripRecs, tripsList)
 			//fmt.Printf("record %+v\n", Record)
 		}
-		i = i + 1
-		if i > 20 {
-			close(TaskQueue)
-			break
-		}
+		i++
+		//if ts > 1 {
+			//break
+		//}
 	}
+	close(TaskQueue)
 }
 
-//ParseRecord parse parameters
-func ParseRecord(s string) []float64 {
+//ParsetoArrayFloat parse parameters
+func ParsetoArrayFloat(s string) []float64 {
 	result := []float64{}
 	for _, element := range strings.Split(s, ",") {
 		p, _ := strconv.ParseFloat(strings.TrimSpace(element), 64)
--- typedef/typedef.go
+++ typedef/typedef.go
@@ -1,14 +1,26 @@
 package typedef
 
+import (
+	"sync"
+)
+
 //Par parameter
 type Par struct {
-	Link, Node, TripTable, LinkClose, TurnProhibit        string
-	NodeTypeZone, NodeTypeEntry, NodeTypeExit, LinkTypeEL []int
-	NumZones, NumThreads                                  int
-	VehClass                                              []string
-	VOT                                                   []float64
-	VOTFactor                                             map[string]float64
-	TollPolicy                                            map[int]map[string]float64
+	Link, Node, TripTable, LinkClose, TurnProhibit, TollConstFile      string
+	ChoiceLogFile                                                      string
+	VehClass, CLogVCLS                                                 []string
+	NodeTypeZone, NumZones, NumThreads, MaxIter, Iter, ELEntry, ELExit int
+	NodeTypeEntry, NodeTypeExit, LinkTypeEL                            []int
+	CLogTS, CLogIter, CLogNode                                         []int
+	SplitCount                                                         [11]int
+	PathBuildCount                                                     [2]int
+	MinSplit, CMReliaRatio, CMReliaTime, CMReliaDist, CMPercTime       float64
+	CMPercMidVC, CMPercMaxVC, CMELWeight, CMScaleLen, CMMaxCir         float64
+	VOT, CMTime, CMToll                                                []float64
+	VOTFactor, CMTimeFactor, CMTollFactor                              map[string]float64
+	TollPolicy                                                         map[int]map[string]float64
+	TollConst                                                          map[int][]float64
+	SmoothForwardPercent, SmoothBackworkPercent, SmoothIter            int
 }
 
 //Link network link
@@ -18,39 +30,48 @@ type Link struct {
 	CAPACITY                                                  int
 	NUMLANES, FTYPE, TOLLPOLICY                               int
 	NODEA, NODEB                                              *Node
-	FFTime, TimeWeight                                        float64 //calculated attributes
-	CgTime, Vol, CgSpeed, TollRate                            []float64
+	FFTime                                                    float64 //calculated attributes
+	CgTime, Vol, Volpre, CgSpeed, TollRate, TimeWeight        [96]float64
+	VolCls, VolClsPre                                         map[string]*[96]float64
+	IsELEntry, IsELExit                                       bool
+}
+
+//Add add volume
+func (l *Link) Add(tp int, v float64, cls string) {
+	var mu sync.RWMutex
+	mu.RLock()
+	defer mu.RUnlock()
+	l.VolCls[cls][tp-1] = l.VolCls[cls][tp-1] + v
 }
 
 //Node network node
 type Node struct {
-	N       int
-	TYPE    int
-	DNGRP   int
-	DNLINKS []*Link
+	N                   int
+	X, Y                float64
+	TYPE                int
+	DNGRP               int
+	DNLINKS             []*Link
+	IsELEntry, IsELExit bool
 }
 
 //PathTask path building task for path builder
 type PathTask struct {
-	TP   int               //time steps
-	I    *Node             //origin node
-	VCLS string            //vehicle class
-	TRIP map[*Node]float64 //list of destinations and trips
+	TP, Rcd        int               //time steps
+	I, O           *Node             //path origin node, packet start zone
+	VCLS           string            //vehicle class
+	TRIP           map[*Node]float64 //list of destinations and trips
+	BanLink        *Link             //banned link for decision node path
+	DisttoDest     float64           //distance to destination of the current path
+	NumSplit       int               //number of split times
+	LShare, HShare float64           //share boundary of packet
 }
 
 //PQItem priority queue node
 type PQItem struct {
-	Nd                            *Node
-	Link                          *Link //parent node link
-	ParentItem                    *PQItem
-	IMP, Time, Dist, Toll, FFTime float64
-	TP, Index                     int
-	Visited                       bool
-}
-
-//PathLink path link
-type PathLink struct {
-	Link    *Link
-	TP      int
-	BNdType int
+	Nd                                     *Node
+	Link                                   *Link //parent node link
+	ParentItem                             *PQItem
+	IMP, Time, PcvTime, Dist, Toll, FFTime float64
+	TP, PqIndex                            int
+	Visited                                bool
 }
